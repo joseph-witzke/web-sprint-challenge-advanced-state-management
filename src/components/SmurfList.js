b5df93fd393ab3dacc347e6ee8d6bc09668df369
Joe Witzke <joewitzke@joes-mbp.lan>
2021-05-14 13:27:43 -0600

    Finished.

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -15,7 +15,6 @@ import { connect } from 'react-redux';
     //     nickname: 'Pops',
     //     description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
     
-
     if (loading) {
         return <h1>Loading...</h1>;
     }
